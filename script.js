ec681bced0960c86cedb251f75c55286fdc1cf44
Shebnem Hajiyeva <hajiyevashebnem@gmail.com>
2025-08-02 11:24:27 -0400

    Created Objects in JS

---
+++ script.js
@@ -0,0 +1,12 @@
+let student={
+    name: "Mike",
+    age: 20,
+    enrolled: true,
+    courses: ["JavaCsript", "Web Programming", "Database"],
+    displayInfo: function(){
+        return `My name is: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}. I enrolled ${this.courses} courses `;
+    }
+};
+console.log("Name:", student.name);
+console.log("Age:", student.age);
+console.log(student.displayInfo());
